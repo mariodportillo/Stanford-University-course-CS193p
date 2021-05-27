7f98c1801dbb1c4b84a73018c18cd2d288d02d2d
Mario D Portillo <mariodportillo@gmail.com>
2021-05-27 10:15:11 -0700

    Add files via upload

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Mario D Portillo on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+                }
+            }
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                Text("👻").font(Font.largeTitle)
+            }else{
+                 RoundedRectangle(cornerRadius: 10.0)
+                     .fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mario D Portillo on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
